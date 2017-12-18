b16a4fb0b982d7d4e32723d94d5dd3c3016908e4
ghhkan <31932572+ghhkan@users.noreply.github.com>
2017-12-17 18:32:36 -0600

    asset
    new

---
+++ js/cyber.js
@@ -0,0 +1,401 @@
+$(document).ready(function(){
+    $("#tag").mouseover(function(){
+        $(this).html("STOP").css("background-color", "red");
+    });
+    $("#tag").mouseleave(function() {
+      $(this).css("background-color", "#212020").html("I was going to fucking bitch that's why worte all shit");
+    });
+    $("#tag").click(function(){
+      $("html, body").animate({
+        scrollTop:$("#containertwo").offset().top
+      }, 900);
+        $("#info").delay(2000).css("opacity", 1);
+        $("#info2").delay(4000).css("opacity", 1);
+        $("#tag16").css("opacity", 0.3);
+        $("#tag17").css("opacity", 0.3);
+        $("#tag18").css("opacity", 0.3);
+        $("#tag19").css("opacity", 0.3);
+        $("#tag20").css("opacity", 0.3);
+        $("#tag21").css("opacity", 0.3);
+        $("#tag22").css("opacity", 0.3);
+        $("#tag23").css("opacity", 0.3);
+    });
+
+/* tag2 */
+    $("#tag2").mouseover(function(){
+          $(this).html("STOP").css("background-color", "red");
+      });
+    $("#tag2").mouseleave(function() {
+        $(this).css("background-color", "#212020").html("我草你妈的，狗就是狗");
+      });
+    $("#tag2").click(function(){
+        $("html, body").animate({
+          scrollTop:$("#containertwo").offset().top
+        }, 900);
+          $("#info").delay(2000).css("opacity", 1);
+          $("#info2").delay(4000).css("opacity", 1);
+          $("#tag16").css("opacity", 0.3);
+          $("#tag17").css("opacity", 0.3);
+          $("#tag18").css("opacity", 0.3);
+          $("#tag19").css("opacity", 0.3);
+          $("#tag20").css("opacity", 0.3);
+          $("#tag21").css("opacity", 0.3);
+          $("#tag22").css("opacity", 0.3);
+          $("#tag23").css("opacity", 0.3);
+      });
+
+/* tag3 */
+    $("#tag3").mouseover(function(){
+            $(this).html("STOP").css("background-color", "red");
+        });
+    $("#tag3").mouseleave(function() {
+          $(this).css("background-color", "#212020").html("誰もあなたが死ぬべきだと思っています、あなたは何を待っていますか？");
+        });
+    $("#tag3").click(function(){
+          $("html, body").animate({
+            scrollTop:$("#containertwo").offset().top
+          }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+        });
+
+/* tag4 */
+    $("#tag4").mouseover(function(){
+              $(this).html("STOP").css("background-color", "red");
+          });
+    $("#tag4").mouseleave(function() {
+            $(this).css("background-color", "#212020").html("都是你说着中国话的日本狗，都是杂种");
+          });
+    $("#tag4").click(function(){
+            $("html, body").animate({
+              scrollTop:$("#containertwo").offset().top
+            }, 900);
+              $("#info").delay(2000).css("opacity", 1);
+              $("#info2").delay(4000).css("opacity", 1);
+              $("#tag16").css("opacity", 0.3);
+              $("#tag17").css("opacity", 0.3);
+              $("#tag18").css("opacity", 0.3);
+              $("#tag19").css("opacity", 0.3);
+              $("#tag20").css("opacity", 0.3);
+              $("#tag21").css("opacity", 0.3);
+              $("#tag22").css("opacity", 0.3);
+              $("#tag23").css("opacity", 0.3);
+          });
+
+/* tag5 */
+    $("#tag5").mouseover(function(){
+                    $(this).html("STOP").css("background-color", "red");
+                });
+    $("#tag5").mouseleave(function() {
+                  $(this).css("background-color", "#212020").html("개소리 몰라");
+                });
+    $("#tag5").click(function(){
+              $("html, body").animate({
+                    scrollTop:$("#containertwo").offset().top
+                  }, 900);
+              $("#info").delay(2000).css("opacity", 1);
+              $("#info2").delay(4000).css("opacity", 1);
+              $("#tag16").css("opacity", 0.3);
+              $("#tag17").css("opacity", 0.3);
+              $("#tag18").css("opacity", 0.3);
+              $("#tag19").css("opacity", 0.3);
+              $("#tag20").css("opacity", 0.3);
+              $("#tag21").css("opacity", 0.3);
+              $("#tag22").css("opacity", 0.3);
+              $("#tag23").css("opacity", 0.3);
+                });
+
+/* tag6 */
+    $("#tag6").mouseover(function(){
+                                $(this).html("STOP").css("background-color", "red");
+                            });
+    $("#tag6").mouseleave(function() {
+                              $(this).css("background-color", "#212020").html("You’re a jerk!");
+                            });
+    $("#tag6").click(function(){
+              $("html, body").animate({
+                                scrollTop:$("#containertwo").offset().top
+                              }, 900);
+             $("#info").delay(2000).css("opacity", 1);
+             $("#info2").delay(4000).css("opacity", 1);
+             $("#tag16").css("opacity", 0.3);
+             $("#tag17").css("opacity", 0.3);
+             $("#tag18").css("opacity", 0.3);
+             $("#tag19").css("opacity", 0.3);
+             $("#tag20").css("opacity", 0.3);
+             $("#tag21").css("opacity", 0.3);
+             $("#tag22").css("opacity", 0.3);
+             $("#tag23").css("opacity", 0.3);
+                            });
+
+/* tag7 */
+    $("#tag7").mouseover(function(){
+        $(this).html("STOP").css("background-color", "red");
+      });
+    $("#tag7").mouseleave(function() {
+        $(this).css("background-color", "#212020").html("너 정 신 병 이 야 ?");
+      });
+    $("#tag7").click(function(){
+        $("html, body").animate({
+          scrollTop:$("#containertwo").offset().top
+        }, 900);
+          $("#info").delay(2000).css("opacity", 1);
+          $("#info2").delay(4000).css("opacity", 1);
+          $("#tag16").css("opacity", 0.3);
+          $("#tag17").css("opacity", 0.3);
+          $("#tag18").css("opacity", 0.3);
+          $("#tag19").css("opacity", 0.3);
+          $("#tag20").css("opacity", 0.3);
+          $("#tag21").css("opacity", 0.3);
+          $("#tag22").css("opacity", 0.3);
+          $("#tag23").css("opacity", 0.3);
+      });
+
+/* tag8 */
+    $("#tag8").mouseover(function(){
+          $(this).html("STOP").css("background-color", "red");
+        });
+    $("#tag8").mouseleave(function() {
+          $(this).css("background-color", "#212020").html("fok jou");
+        });
+    $("#tag8").click(function(){
+          $("html, body").animate({
+            scrollTop:$("#containertwo").offset().top
+          }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+        });
+
+/* tag9 */
+    $("#tag9").mouseover(function(){
+                  $(this).html("STOP").css("background-color", "red");
+                });
+    $("#tag9").mouseleave(function() {
+                  $(this).css("background-color", "#212020").html("一群猪，没文化没教养，真是什么样的爹妈生出什么样的杂种");
+                });
+    $("#tag9").click(function(){
+            $("html, body").animate({
+                    scrollTop:$("#containertwo").offset().top
+                  }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+                });
+
+/* tag10 */
+  $("#tag10").mouseover(function(){
+                              $(this).html("STOP").css("background-color", "red");
+                            });
+  $("#tag10").mouseleave(function() {
+                              $(this).css("background-color", "#212020").html("το μουνη τις μανας σου");
+                            });
+  $("#tag10").click(function(){
+            $("html, body").animate({
+                                scrollTop:$("#containertwo").offset().top
+                              }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+          });
+
+/* tag11 */
+  $("#tag11").mouseover(function(){
+        $(this).html("STOP").css("background-color", "red");
+          });
+ $("#tag11").mouseleave(function() {
+        $(this).css("background-color", "#212020").html("Piss off");
+          });
+$("#tag11").click(function(){
+            $("html, body").animate({
+              scrollTop:$("#containertwo").offset().top
+            }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+          });
+
+/* taag12 */
+  $("#tag12").mouseover(function(){
+                $(this).html("STOP").css("background-color", "red");
+                  });
+  $("#tag12").mouseleave(function() {
+                $(this).css("background-color", "#212020").html("Everyone hate YOU!!!!");
+                  });
+  $("#tag12").click(function(){
+            $("html, body").animate({
+                      scrollTop:$("#containertwo").offset().top
+                    }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+                  });
+
+/* tag13 */
+  $("#tag13").mouseover(function(){
+                $(this).html("STOP").css("background-color", "red");
+                      });
+  $("#tag13").mouseleave(function() {
+                $(this).css("background-color", "#212020").html("死ぬゴーゴー");
+                      });
+  $("#tag13").click(function(){
+            $("html, body").animate({
+                          scrollTop:$("#containertwo").offset().top
+                        }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+                      });
+
+/* tag14 */
+  $("#tag14").mouseover(function(){
+          $(this).html("STOP").css("background-color", "red");
+                                          });
+  $("#tag14").mouseleave(function() {
+          $(this).css("background-color", "#212020").html("Looks like yew lost weight. What are yew now, 5,000 pounds?");
+                                          });
+  $("#tag14").click(function(){
+            $("html, body").animate({
+                        scrollTop:$("#containertwo").offset().top
+                        }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+        });
+
+/* tag15 */
+$("#tag15").mouseover(function(){
+            $(this).html("STOP").css("background-color", "red");
+              });
+  $("#tag15").mouseleave(function() {
+          $(this).css("background-color", "#212020").html(">肥え太った乞食ｗ");
+          });
+  $("#tag15").click(function(){
+            $("html, body").animate({
+              scrollTop:$("#containertwo").offset().top
+            }, 900);
+            $("#info").delay(2000).css("opacity", 1);
+            $("#info2").delay(4000).css("opacity", 1);
+            $("#tag16").css("opacity", 0.3);
+            $("#tag17").css("opacity", 0.3);
+            $("#tag18").css("opacity", 0.3);
+            $("#tag19").css("opacity", 0.3);
+            $("#tag20").css("opacity", 0.3);
+            $("#tag21").css("opacity", 0.3);
+            $("#tag22").css("opacity", 0.3);
+            $("#tag23").css("opacity", 0.3);
+          });
+
+$("#click").click(function(){
+  $(this).addClass("onclick").fadeOut("slow");
+  $("#tag").animate({
+    opacity:'1'
+  });
+  $("#tag2").delay(1000).animate({
+    opacity:'1'
+  });
+  $("#tag3").delay(2000).animate({
+    opacity:'1'
+  });
+  $("#tag4").delay(3000).animate({
+    opacity:'1'
+  });
+  $("#tag5").delay(4000).animate({
+    opacity:'1'
+  });
+  $("#tag6").delay(5000).animate({
+    opacity:'1'
+  });
+  $("#tag7").delay(6000).animate({
+    opacity:'1'
+  });
+  $("#tag8").delay(7000).animate({
+    opacity:'1'
+  });
+  $("#tag9").delay(8000).animate({
+    opacity:'1'
+  });
+  $("#tag10").delay(9000).animate({
+    opacity:'1'
+  });
+  $("#tag11").delay(10000).animate({
+    opacity:'1'
+  });
+  $("#tag12").delay(11000).animate({
+    opacity:'1'
+  });
+  $("#tag13").delay(12000).animate({
+    opacity:'1'
+  });
+  $("#tag14").delay(13000).animate({
+    opacity:'1'
+  });
+  $("#tag15").delay(14000).animate({
+    opacity:'1'
+  });
+  $("#hit").delay(16000).animate({
+    opacity:'1'
+  });
+});
+
+
+
+
+});
